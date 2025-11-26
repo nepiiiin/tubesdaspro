0c2bb52e81acf3ee5d6cc94873c9c2e0ce939b27
Muhammad Lukman Toro <torolukman355@gmail.com>
2025-11-26 21:10:55 +0700

    yang baru

---
+++ belajar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>  
+
+int main(){
+    int i, n;
+    printf ("contoh perurnagan \n");
+    printf ("masukkan nilai n: ");
+    scanf ("%d", &n);   
+
+    for (i=1; i <= 10; i++){
+        for (int n=1; n <=10; n++ ){  
+            printf ("%3d ", i*n);    
+    }
+    
+    printf ("\n"); }
+    
+}
